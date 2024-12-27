9a328a3538a881697a9decffc568e9d92e185763
vnitpankaj <mt23aai037@students.vnit.ac.in>
2024-12-27 20:00:46 +0530

    Update calssf_model.py
    add train test split to 0.25

--- calssf_model.py
+++ calssf_model.py
@@ -14,7 +14,7 @@ y = iris.target
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X, y, test_size=0.25, random_state=42
 )
 
 # Initialize XGBoost classifier
